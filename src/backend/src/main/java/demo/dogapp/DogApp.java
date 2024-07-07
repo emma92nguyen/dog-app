b6e6e5c4964f85b7466c6e9206c6819a1c8b631e
emma92nguyen <emma92nguyen@gmail.com>
2024-07-07 21:09:12 +0200

    initial Backend

---
+++ src/backend/src/main/java/demo/dogapp/DogApp.java
@@ -0,0 +1,11 @@
+package demo.dogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DogApp.class, args);
+    }
+}
